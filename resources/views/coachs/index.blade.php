811c153a1161a2f5f9c0744b6d61651d49660ae6
ccharaco <jcesarchg9@gmail.com>
2019-07-31 13:18:41 -0400

    Index de coach y jugadores

---
+++ resources/views/coachs/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header float-left">
+            <div class="page-title">
+                <h1>Coachs</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="page-header float-right">
+            <div class="page-title">
+                <ol class="breadcrumb text-right">
+                    <li class="active">Listado</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="content mt-3">
+    <div class="animated fadeIn">
+        <div class="row">
+
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong class="card-title">Listado de coachs</strong>
+                    <a href="{{ route('coachs.create') }}"><button type="button" class="btn btn-primary btn-sm pull-right"><i class="fa fa-star"></i> Registrar</button></a>
+                </div>
+                <div class="card-body">
+          <table id="bootstrap-data-table" class="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>RUT</th>
+                <th>Edad</th>
+                <th>Equipo</th>
+                <th>Opciones</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach($coachs as $key)
+                    <tr>
+                        <td>{{ $key->nombres }}</td>
+                        <td>{{ $key->apellidos }}</td>
+                        <td>{{ $key->rut}}</td>
+                        <td>{{ $key->edad }}</td>
+                        <td>{{ $key->equipos->nombre }}</td>
+                        <td align="center">
+                            <a href="#"><button type="button" class="btn btn-primary btn-sm pull"><i class="fa fa-edit"></i></button></a>
+
+                            <a href="#"><button type="button" class="btn btn-danger btn-sm pull"><i class="fa fa-trash"></i></button></a>
+
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+          </table>
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+    </div><!-- .animated -->
+</div><!-- .content -->
+@endsection
+
