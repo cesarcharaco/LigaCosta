6dcdef769043a2379e9bea75c330656765282d45
antonileon <antonijleon@gmail.com>
2019-07-31 13:19:29 -0430

    create de equipos

--- resources/views/equipos/create.blade.php
+++ resources/views/equipos/create.blade.php
@@ -27,29 +27,42 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
-                  <div class="card-header">
-                    <strong>Horizontal</strong> Form
-                  </div>
-                  <div class="card-body card-block">
-                    <form action="" method="post" class="form-horizontal">
-                      <div class="row form-group">
-                        <div class="col col-md-3"><label for="hf-email" class=" form-control-label">Email</label></div>
-                        <div class="col-12 col-md-9"><input type="email" id="hf-email" name="hf-email" placeholder="Enter Email..." class="form-control"><span class="help-block">Please enter your email</span></div>
-                      </div>
-                      <div class="row form-group">
-                        <div class="col col-md-3"><label for="hf-password" class=" form-control-label">Password</label></div>
-                        <div class="col-12 col-md-9"><input type="password" id="hf-password" name="hf-password" placeholder="Enter Password..." class="form-control"><span class="help-block">Please enter your password</span></div>
-                      </div>
-                    </form>
-                  </div>
-                  <div class="card-footer">
-                    <button type="submit" class="btn btn-primary btn-sm">
-                      <i class="fa fa-dot-circle-o"></i> Submit
-                    </button>
-                    <button type="reset" class="btn btn-danger btn-sm">
-                      <i class="fa fa-ban"></i> Reset
-                    </button>
-                  </div>
+                    <div class="card-header">
+                        <strong>Registro de equipo</strong>
+                    </div>
+                    <div class="card-body card-block">
+                        <form action="" method="post" class="form-horizontal">
+                            <div class="row form-group">
+                                <div class="col col-md-3"><label for="nombre" class=" form-control-label">Nombre</label></div>
+                                <div class="col-12 col-md-9"><input type="text" id="nombre" name="nombre" placeholder="Ingrese nombre..." class="form-control"></div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col col-md-3"><label for="lema" class=" form-control-label">Lema</label></div>
+                                <div class="col-12 col-md-9"><input type="text" id="lema" name="lema" placeholder="Ingrese lema..." class="form-control"></div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col col-md-3"><label for="liga" class=" form-control-label">Liga</label></div>
+                                <div class="col-12 col-md-9">
+                                    <select name="liga" id="liga" class="form-control">
+                                        <option value="Football Tackle">Football Tackle</option>
+                                        <option value="Football flag">Football Flag</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row form-group">
+                                <div class="col col-md-3"><label for="logo" class=" form-control-label">Logo</label></div>
+                                <div class="col-12 col-md-9"><input type="file" id="logo" name="logo" placeholder="Ingrese logo..." class="form-control"></div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary btn-sm">
+                                  <i class="fa fa-dot-circle-o"></i> Registrar
+                                </button>
+                                <button type="reset" class="btn btn-danger btn-sm">
+                                  <i class="fa fa-ban"></i> Cancelar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
